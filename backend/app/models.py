ec7a53c0c9d050e2c21c084f3971c061caf67b34
Luis Felipe <vilella.luisfelipe@gmail.com>
2020-10-14 08:03:54 -0300

    start fix tables relationship

--- backend/app/models.py
+++ backend/app/models.py
@@ -32,11 +32,27 @@ class Item(Base):
         sqlalchemy.Integer, primary_key=True, unique=True, index=True
     )
     name = sqlalchemy.Column(sqlalchemy.String)
-    price = sqlalchemy.Column(sqlalchemy.Float)
     measure = sqlalchemy.Column(sqlalchemy.String)
+
+
+class OrderItem(Base):
+    __tablename__ = 'orderitems'
+
+    id = sqlalchemy.Column(
+        sqlalchemy.String,
+        primary_key=True,
+        unique=True,
+        index=True,
+        default=lambda: str(uuid.uuid4()),
+    )
     order_id = sqlalchemy.Column(
         sqlalchemy.String, sqlalchemy.ForeignKey('orders.id')
     )
+    item_id = sqlalchemy.Column(
+        sqlalchemy.String, sqlalchemy.ForeignKey('items.id')
+    )
+    price = sqlalchemy.Column(sqlalchemy.Float)
+    discount = sqlalchemy.Column(sqlalchemy.Float)
 
 
 class Order(Base):
@@ -49,10 +65,10 @@ class Order(Base):
         index=True,
         default=lambda: str(uuid.uuid4()),
     )
-    user = sqlalchemy.Column(
+    user_id = sqlalchemy.Column(
         sqlalchemy.String, sqlalchemy.ForeignKey('users.id')
     )
-    items = sqlalchemy.orm.relationship('Item')
+    items = sqlalchemy.orm.relationship('OrderItem')
     is_active = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
     created_at = sqlalchemy.Column(
         sqlalchemy.DateTime, default=datetime.datetime.utcnow
