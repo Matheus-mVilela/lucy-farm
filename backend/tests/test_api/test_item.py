08357c8cf7df7e36ff756ac8db4b7d4cf86c43af
Matheus_Vilela <matheus_vilela@icloud.com>
2020-10-21 19:32:22 -0300

    create test API TestCreateItem

--- backend/tests/test_api/test_item.py
+++ backend/tests/test_api/test_item.py
@@ -19,11 +19,22 @@ class TestCreateItem:
     def test_valid_returns_complete_body(self, payload_item):
         response = client.post(self.build_url(), json=payload_item)
 
-        breakpoint()
-
         assert response.json() == {
             "name": payload_item["name"],
             "price": payload_item["price"],
             "measure": payload_item["measure"],
             "id": "1",
         }
+
+    def test_valid_saves_on_db(self, payload_item, session_maker):
+        assert session_maker().query(models.Item).count() == 0
+
+        response = client.post(self.build_url(), json=payload_item)
+        assert response.ok
+
+        assert session_maker().query(models.Item).count() == 1
+
+        item = session_maker().query(models.Item).first()
+        assert payload_item["name"] == item.name
+        assert payload_item["price"] == item.price
+        assert payload_item["measure"] == item.measure
